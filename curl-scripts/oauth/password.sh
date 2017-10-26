5e0620f01b19dd16769cfeac7eec8bd489686338
Prabath Siriwardana <prabath@wso2.com>
2017-10-26 02:00:26 -0600

    adding new scripts

--- curl-scripts/oauth/password.sh
+++ curl-scripts/oauth/password.sh
@@ -1,1 +1,1 @@
-curl -v -X POST --basic -u $CLIENTID:$CLIENTSECRET -H "Content-Type: application/x-www-form-urlencoded;charset=UTF-8" -k -d "grant_type=password&username=$USERNAME&password=$PASSWORD&scope=$SCOPE” $TOKEN_EP
+curl -v -X POST --basic -u $CLIENTID:$CLIENTSECRET -H "Content-Type: application/x-www-form-urlencoded;charset=UTF-8" -k -d "grant_type=password&username=$USERNAME&password=$PASSWORD&scope=$SCOPE" $TOKEN_EP
