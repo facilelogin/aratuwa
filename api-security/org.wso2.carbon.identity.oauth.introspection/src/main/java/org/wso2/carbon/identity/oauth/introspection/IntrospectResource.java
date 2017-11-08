1d2fd91e578ab845531b093a922bf84d85780356
dileepagayan <dileepafit@gmail.com>
2017-11-08 15:03:17 +0530

    Refactored the imports

--- api-security/org.wso2.carbon.identity.oauth.introspection/src/main/java/org/wso2/carbon/identity/oauth/introspection/IntrospectResource.java
+++ api-security/org.wso2.carbon.identity.oauth.introspection/src/main/java/org/wso2/carbon/identity/oauth/introspection/IntrospectResource.java
@@ -9,7 +9,12 @@ import org.wso2.carbon.identity.oauth2.OAuth2TokenValidationService;
 import org.wso2.carbon.identity.oauth2.dto.OAuth2IntrospectionResponseDTO;
 import org.wso2.carbon.identity.oauth2.dto.OAuth2TokenValidationRequestDTO;
 
-import javax.ws.rs.*;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
